d6b4098ed3b6038612c8dfc2574a8723fe5df056
Luiz Vinícius <viniciusadr244@gmail.com>
2022-08-27 18:32:07 -0300

    ok

--- djangoServer/eleicao/views.py
+++ djangoServer/eleicao/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {'name': 'Biroliro'})
+
+
+def add(request):
+
+    num1 = int(request.GET['num1'])
+    num2 = int(request.GET['num2'])
+    res = num1 + num2
+
+    return render(request, 'result.html', {'result': res})
